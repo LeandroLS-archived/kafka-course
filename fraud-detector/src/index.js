48f46705609e4ea8f2b4d0b813b3e1bdfcf41bb6
Leandro Lima <llimas@outlook.com>
2021-09-05 13:11:03 -0300

    aumentando numero de partições

--- fraud-detector/src/index.js
+++ fraud-detector/src/index.js
@@ -9,10 +9,10 @@ async function run() {
     await consumer.subscribe({ topic: 'ECOMMERCE_NEW_ORDER', fromBeginning: true })
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
+            console.log('---------- Fraud Detector ------')
             console.log('Processing new order, cheking for fraud')
-            console.log({
-                value: message.value.toString(),
-            })
+            console.log(`Partition ${partition}`)
+            console.log(`Message ${message.value.toString()}`)
             console.log('Order processed')
         },
     }).catch(e => console.log('DEU RUIM', e))
